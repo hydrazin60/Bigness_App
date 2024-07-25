808d36b1819ef1fac9a58dbf97a48bd4f189e808
hydrazin60 <sureshkhdk45@gmail.com>
2024-07-25 19:38:41 +0545

    custem font used

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -3,12 +3,32 @@ import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.font}>Home</Text>
+      <Text style={styles.font1}>Home</Text>
+      <Text style={styles.font2}>Home</Text>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  font: {
+    fontSize: 40,
+    fontFamily: " outfit",
+  },
+  font1: {
+    fontSize: 40,
+    fontFamily: "outfit-midium",
+  },
+  font2: {
+    fontSize: 40,
+    fontFamily: "utregularfit",
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
-export default function _layoutt() {
+export default function RootLayout() {
+  useFonts({
+    outfit: require("./../assets/fonts/Outfit-Bold.ttf"),
+    "outfit-midium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    outregularfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+  });
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
